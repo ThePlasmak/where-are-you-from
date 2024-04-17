14051134c6788e531c6f25087387a816af838b9d
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-04-18 00:11:22 +0800

    Adjust build process

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
 call tweego -l -o export/where_are_you_from.html src
-python scripts/loading_screen_remover.py "export/where_are_you_from.html"
+python python_scripts/loading_screen_remover.py "export/where_are_you_from.html"
+
 call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Where_Are_You_From.html" src
-python scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Where_Are_You_From.html"
+python python_scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Where_Are_You_From.html"
+
+python python_scripts/twee_to_poof.py
+
+python python_scripts/twee_to_docx.py
