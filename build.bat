8d5c8d239b6009de8f8e99fd1ddffcdf7878d05b
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-04-17 23:56:14 +0800

    Add loading screen remover

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
 call tweego -l -o export/where_are_you_from.html src
+python scripts/loading_screen_remover.py "export/where_are_you_from.html"
+call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Where_Are_You_From.html" src
+python scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Where_Are_You_From.html"
