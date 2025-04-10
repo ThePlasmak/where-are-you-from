076704198e48706f12880ff653b427d8322bbb3a
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2025-04-10 17:49:08 +0800

    Improve first page UI and refresh page when input is empty

--- export.bat
+++ export.bat
@@ -4,6 +4,6 @@ python python_scripts/loading_screen_remover.py "export/Where_Are_You_From.html"
 call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Where_Are_You_From.html" src
 python python_scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Where_Are_You_From.html"
 
-python python_scripts/twee_to_poof.py
+@REM python python_scripts/twee_to_poof.py
 
-python python_scripts/twee_to_docx.py
+@REM python python_scripts/twee_to_docx.py
